9c1e07d9e93d5462af63b7350954e23998fc391b
DashaPrincess <61591826+DashaPrincess@users.noreply.github.com>
2021-12-21 19:41:15 +0300

    отимпортировала store в Index.tsx

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -5,8 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import store from './store/store';
 
-// const store = createStore();
+
 
 ReactDOM.render(
   <React.StrictMode>
