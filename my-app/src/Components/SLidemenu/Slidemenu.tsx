b43e25b4c8b3e4881503a1a6440cd9bced18e33c
AbushaevaDiana <abu13.di@gmail.com>
2021-12-23 22:28:39 +0300

    slidemenu добавление mapState и mapDispath

--- my-app/src/Components/SLidemenu/Slidemenu.tsx
+++ my-app/src/Components/SLidemenu/Slidemenu.tsx
@@ -1,12 +1,10 @@
 import '../../App.css';
 import styles from './Slidemenu.module.css';
 import { connect } from 'react-redux';
-import React, { useEffect, useRef, useState } from 'react';
-import { Slide } from '../../types';
+import { Presentation, Slide } from '../../types';
 import { SlideView } from '../Slide/Slide';
-import { isPropertySignature } from 'typescript';
-
-
+import { addSlide, deleteSlide, gotoSlide } from '../../store/actionsCreators/slideActionCreators'
+import  store  from '../../store/store'
 
 interface SlidemenuList {
     slidelist: Array<Slide>
@@ -19,8 +17,20 @@ export function Slidemenu(props: SlidemenuList){
                <ul className={styles.slidemenuList}> 
                  {props.slidelist.map(slide => <SlideView slide = {slide}></SlideView>)}
                </ul>
-                   <div className={styles.imglogo}></div>
+                   <div className={styles.imglogo} ></div>
                </div>
             </>
         )
 };
+
+//
+function mapStateToProps(state: Presentation) {
+    return { props: state.slidelist} 
+};
+
+const mapDispatchToProps = {
+    addSlide,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Slidemenu);
+//
