acae0b056b6d4a0c07fc092783418c42ad75a3e8
AbushaevaDiana <abu13.di@gmail.com>
2022-01-04 20:12:09 +0300

    наврала в преведущем коммите, было сломанно, починила

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -8,11 +8,9 @@ import { initialState } from './test/initialState';
 import { Presentation, PresentationMaker, SelectionType } from './types';
 import { connect } from 'react-redux';
 import { addSlide, deleteSlide, gotoSlide } from './store/actionsCreators/slideActionCreators'
-import { addActionToHistory } from './store/actionsCreators/historyActionCreators'
 
 interface AppProps {
   gotoSlide: (idSlide: Number) => void,
-  addActionToHistory: (presentation: Presentation, selection: SelectionType) => void,
   state: PresentationMaker,
 }
 
@@ -37,7 +35,6 @@ function mapStateToProps(state: PresentationMaker) {
 
 const mapDispatchToProps = {
   gotoSlide,
-  addActionToHistory
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
