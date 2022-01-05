68fd148a6423dc41544768538ecd8d6bc4e0ef2a
AbushaevaDiana <abu13.di@gmail.com>
2022-01-05 14:21:14 +0300

    drag and drop попытки

--- my-app/src/Components/Element/Element.tsx
+++ my-app/src/Components/Element/Element.tsx
@@ -2,7 +2,7 @@ import '../../App.css';
 // import styles from './Slide.module.css';
 import { PresentationMaker, SelectionType, Slide, Color, ElementType, Size, Position } from '../../types';
 import { connect } from 'react-redux';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { DragEvent, useEffect, useRef, useState } from 'react';
 import { changeTextContent } from '../../store/actionsCreators/elementActionCreators'
 
 interface ElementProps{
@@ -14,12 +14,26 @@ interface ElementProps{
 
 
 export function Element(props: ElementProps){    
-    function dragStartHandler(e: DragEvent, props: ElementProps) {
+    const[currentElement, setCurrentElement] = useState(props.element)
+    function dragStartHandler(e: DragEvent<HTMLDivElement>, props: ElementProps) {
+        console.log('drag', props.element)
+        setCurrentElement(props.element)
+    }
+    function dragLeaveHandler(e: DragEvent<HTMLDivElement>, props: ElementProps) {
+        e.preventDefault()
 
     }
+    function dragEndHandler(e: DragEvent<HTMLDivElement>, props: ElementProps) {
 
+    }
+    function dragOverHandler(e: DragEvent<HTMLDivElement>, props: ElementProps) {
+        e.preventDefault()
 
-
+    }
+    function dropHandler(e: DragEvent<HTMLDivElement>, props: ElementProps) {
+        e.preventDefault()
+ 
+    }
 
 
     let textI: string = '';   
@@ -28,11 +42,11 @@ export function Element(props: ElementProps){
        return (
         <>
           <div 
-          onDragStart={(e) => dragStartHandler(e, props)}
-          onDragLeave={}
-          onDragEnd={}
-          onDragOver={}
-          onDrop={}
+          onDragStart={(e: DragEvent<HTMLDivElement>) => dragStartHandler(e, props)}
+          onDragLeave={(e: DragEvent<HTMLDivElement>) => dragLeaveHandler(e, props)}
+          onDragEnd={(e: DragEvent<HTMLDivElement>) => dragEndHandler(e, props)}
+          onDragOver={(e: DragEvent<HTMLDivElement>) => dragOverHandler(e, props)}
+          onDrop={(e: DragEvent<HTMLDivElement>) => dropHandler(e, props)}
           draggable = {true}>
             <svg height="100" width="100">
                 <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />
